373c8290771a220c85c533bd8ef99d07aef0c22b
Léonard Wojcik <leonardw@tutanota.com>
2021-10-28 18:17:20 +0200

    feat: close modal on click - Close the modal of the user clicks on the "x" icon or outside of the modal box.

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeButton = document.querySelector(".close")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,19 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// Close the modal after clicking on the "x" icon.
+closeButton.onclick = function() {
+  modalbg.style.display = "none";
+}
+
+// Close the modal if the user clicks outside of the modal.
+window.onclick = function(event) {
+  if (event.target == modalbg) {
+    modalbg.style.display = "none";
+  }
+} 
+
+// TODO : change name of the "bground" to something more explicit (modal).
 
 
