11662f69e9b94bce0aaf8ff58ae22c8196ec3bab
Léonard Wojcik <leonardw@tutanota.com>
2021-11-02 14:32:38 +0100

    feat: set form validation. - Set Built-in form validation (HTML5). - Set JavaScript form validation. - Removed onsubmit="return validate(); in HTML (form tag). - Set html document language to fr.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 2. Il est conseillé d'utiliser VisualStudio Code et vous pouvez utiliser Docker, mais ce n'est pas obligatoire ;
 3. Il n'y a aucune dépendance ;
 4. Vous ne devez utiliser que du CSS personnalisé et du JavaScript pur, sans jQuery, Bootstrap ou autre librairie.
+
+## Test the JavaScript form validation
+
+In order to check if the JavaScript used for checking the form is valid, add the "novalidate" attribute in the balise form, like this :
+
+<code><form id="contact-form" name="reserve" action="index.html" method="get" novalidate></code>
+
+(don't forget to delete this word after your tests in order to enable the double verification !)
--- contactForm.js
+++ contactForm.js
@@ -1,9 +1,47 @@
+/////////////////////////////
+// Form fields validation //
+///////////////////////////
+
+let firstName = document.getElementById("first-name");
+
+////////////////////////////
+// Form submit validation //
+///////////////////////////
+
 // Retrieve DOM data of the form
 
 let contactForm = document.getElementById("contact-form");
 
 // Send an alert message when the form has been completed
 contactForm.addEventListener("submit", (e) => {
-    e.preventDefault(); // prevent validation if the form is not complete.
-    alert('Merci pour votre inscription !');
+    // prevent validation if the form is not complete.
+    e.preventDefault();
+    // retrieve all "input" tags with a "required attribute"
+    let fields = document.querySelectorAll("input[required]");
+    // this variable will be set to "false" if one field is not valid
+    let valid = true;
+
+    // Check validity of every field
+    fields.forEach((field) => {
+        // If the field is not valid, return false
+        if (! validateField(field)){
+            valid = false;
+        }
+    } );
+    // If the field is valid, submit form and display message
+    if(valid){
+        e.target.submit();
+        alert('Merci pour votre inscription !');
+    }
+
+
 }, false);
+
+// Check if a field is valid or not.
+function validateField(field){
+    if (field.checkValidity()){
+        return true;
+    } else {
+        return false;
+    }
+}
