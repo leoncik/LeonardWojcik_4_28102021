48936b00d592c35f6d86af20f9f0515f7ac5c004
Léonard Wojcik <leonardw@tutanota.com>
2021-11-17 13:55:35 +0100

    refactor: JS nav click event in separate file - Put the onclick event in HTML in a separate JS file. - Add margin before tournament quantity description. - Remove font folder (fonts are called with a google fonts link in the head).

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,10 @@
-// Hamburger menu behaviour
+// Launch hamburger menu on click
+const menuIcon = document.getElementsByClassName("icon");
+menuIcon[0].addEventListener('click', (e) => {
+  editNav();
+}, false)
 
+// Hamburger menu behaviour
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
